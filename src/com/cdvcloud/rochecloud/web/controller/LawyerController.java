66109f87afc3d490dbd2934ad7c779f0156ac526
readyhuihui <1799173516@qq.com>
2018-08-11 17:57:51 +0800

    修改注解

--- src/com/cdvcloud/rochecloud/web/controller/LawyerController.java
+++ src/com/cdvcloud/rochecloud/web/controller/LawyerController.java
@@ -1,10 +1,14 @@
 package com.cdvcloud.rochecloud.web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @Author:lyh
  * @Description:
  * @Date:Created in 2018/8/11 17:49
  */
 @Controller
+@RequestMapping(value = "lawyer/")
 public class LawyerController {
 }
